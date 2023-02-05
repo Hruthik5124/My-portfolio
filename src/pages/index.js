6afe4e4d60e0b26f125cc18b1699c6ee7bf6e53e
Hruthik5124 <hruthik5124@gmail.com>
2023-02-05 18:08:37 +0530

    Added IN animation to components

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,27 @@ import { useInView } from "react-intersection-observer";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true); 
-   
+  const isBrowser = () => typeof window !== "undefined"
+
+useEffect(()=>{window.addEventListener('scroll', reveal);}, []);
+  
+
+
+  function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+    for(var i=0; i< reveals.length; i++){
+      var windowheight =window.innerHeight;
+      var reveal = reveals[i].getBoundingClientRect().top;
+      var revealpoint = 150;
+      if(reveal < windowheight - revealpoint){
+        reveals[i].classList.add('active');
+      }
+      else{
+        reveals[i].classList.remove('active');
+      }
+    }
+  }
+
 
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -56,9 +76,11 @@ export default function Home() {
           </nav>
           <div className="text-center p-10 py-10">
             <section className="hruthik">
+              <section>
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
               Hruthik
             </h2>
+            </section>
             </section>
             <section>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
@@ -76,14 +98,25 @@ export default function Home() {
               <a href="https://www.linkedin.com/in/hruthik-shetty-3983b41aa/" target="_blank"><AiFillLinkedin /></a>
               <a href="https://www.instagram.com/hruthikshetty/" target="_blank"><AiFillInstagram /></a>
             </div>
+            <section class="arrow">
+            <div className="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full relative overflow-hidden mx-auto mt-20">
+                <svg class="w-6 h-6 text-violet-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                  <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path></svg>
+            </div>
+            </section>
+            <section class="reveal">
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-60 h-60 relative overflow-hidden mt-10 md:h-96 md:w-96">
               <Image src={deved} layout="fill" objectFit="cover" />
             </div>
+            </section>
           </div>
         </section>
         <section>
           <div>
+            <section class="reveal">
             <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
+            </section>
+            <section class="reveal">
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Since the beginning of my journey as a freelance designer and
               developer, I've done remote work for
@@ -92,60 +125,66 @@ export default function Home() {
               and collaborated with talanted people to create digital products
               for both business and consumer use.
             </p>
+            </section>
+            <section class="reveal">
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               I offer from a wide range of services, including brand design,
               programming and teaching.
             </p>
+            </section>
           </div>
+          <div class="reveal">
           <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-black flex-1">
               <Image src={design} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2  ">
+              <h3 className="text-lg font-medium pt-8 pb-2 dark:text-white ">
                 Beautiful Designs
               </h3>
-              <p className="py-2">
+              <p className="py-2 dark:text-white">
                 Creating elegant designs suited for your needs following core
                 design theory.
               </p>
               <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <p className="text-gray-800 py-1 dark:text-gray-400">Photoshop</p>
+              <p className="text-gray-800 py-1 dark:text-gray-400">Illustrator</p>
+              <p className="text-gray-800 py-1 dark:text-gray-400">Figma</p>
+              <p className="text-gray-800 py-1 dark:text-gray-400">Indesign</p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-black flex-1">
               <Image src={code} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">
+              <h3 className="text-lg font-medium pt-8 pb-2 dark:text-white ">
                 Code your dream project
               </h3>
-              <p className="py-2">
+              <p className="py-2 dark:text-white ">
                 Do you have an idea for your next great website? Let's make it a
                 reality.
               </p>
               <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <p className="text-gray-800 py-1 dark:text-gray-400">Photoshop</p>
+              <p className="text-gray-800 py-1 dark:text-gray-400">Illustrator</p>
+              <p className="text-gray-800 py-1 dark:text-gray-400">Figma</p>
+              <p className="text-gray-800 py-1 dark:text-gray-400">Indesign</p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-black flex-1">
               <Image src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-              <p className="py-2">
+              <h3 className="text-lg font-medium pt-8 pb-2 dark:text-white ">Consulting</h3>
+              <p className="py-2 dark:text-white ">
                 Are you interested in feedback for your current project? I can
                 give you tips and tricks to level it up.
               </p>
               <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <p className="text-gray-800 py-1 dark:text-gray-400">Photoshop</p>
+              <p className="text-gray-800 py-1 dark:text-gray-400">Illustrator</p>
+              <p className="text-gray-800 py-1 dark:text-gray-400">Figma</p>
+              <p className="text-gray-800 py-1 dark:text-gray-400">Indesign</p>
             </div>
+          </div>
           </div>
         </section>
         <section className="py-10">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
+            <section class="reveal"><h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3></section>
+            <div class="reveal">
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Since the beginning of my journey as a freelance designer and
               developer, I've done remote work for
@@ -154,11 +193,15 @@ export default function Home() {
               and collaborated with talanted people to create digital products
               for both business and consumer use.
             </p>
+            </div>
+            <section class="reveal">
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               I offer from a wide range of services, including brand design,
               programming and teaching.
             </p>
+            </section>
           </div>
+          <section class="reveal">
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1 ">
               <Image
@@ -215,6 +258,7 @@ export default function Home() {
               />
             </div>
           </div>
+          </section>
         </section>
       </main>
     </div>
